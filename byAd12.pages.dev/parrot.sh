c3ead0fb70479bec8d4715a9c56ca576485d4d16
byAd12 <92215714+byAd12@users.noreply.github.com>
2026-01-14 23:33:14 +0100

    arreglo de sintaxis y estilos

---
+++ byAd12.pages.dev/parrot.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# -- byad12.pages.dev/parrot.sh
+# -- Script hecho por Adrián L. G. P.
+
+##############################################################
+# ACTUALIZAR E INSTALAR PAQUETES
+##############################################################
+apt update
+apt install -y bat openvpn neofetch htop micro cowsay
+clear
+
+##############################################################
+# AÑADIR ALIAS
+##############################################################
+anadir_alias() {
+    local alias="$1"
+    local comando="$2"
+    if ! grep -q "alias $alias=" ~/.bashrc; then
+        echo "alias $alias=\"$comando\"" >> ~/.bashrc
+
+        echo "[+] alias $alias='$comando' >> ~/.bashrc"
+    else
+        echo "[!] alias $alias='$comando' >> ~/.bashrc"
+    fi
+}
+
+anadir_alias nano "micro"
+anadir_alias cat "batcat"
+anadir_alias vpn "sudo /usr/sbin/openvpn --config"
+anadir_alias py_http_8000 'sudo python3 -m http.server 8000'
+
+##############################################################
+# AÑADIR COMANDO DE INICIO
+##############################################################
+anadir_comando() {
+    local comando="$1"
+    if ! grep -q "$comando" ~/.bashrc; then
+        echo "$comando" >> ~/.bashrc
+        echo "[+] $comando >> ~/.bashrc"
+    else
+        echo "[!] $comando >> ~/.bashrc"
+    fi
+}
+
+anadir_comando "neofetch"
+
+##############################################################
+# DIBUJO
+##############################################################
+/usr/games/cowsay -f tux byAd12
