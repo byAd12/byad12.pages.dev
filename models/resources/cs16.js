89dcc05c3e19d5f799c595f2e060a4f3f3527da4
byAd12 <92215714+byAd12@users.noreply.github.com>
2026-02-23 19:50:56 +0100

    Limpieza de historial

---
+++ models/resources/cs16.js
@@ -0,0 +1,160 @@
+function ensenar_multiplayer() {
+  document.getElementById("opcion_contenido_multiplayer").style.display = "block";
+  document.getElementById("button_multiplayer").classList.add("seleccionado");
+
+  document.getElementById("opcion_contenido_audio").style.display = "none";
+  document.getElementById("button_audio").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_video").style.display = "none";
+  document.getElementById("button_video").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_aim").style.display = "none";
+  document.getElementById("button_aim").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_keyboard").style.display = "none";
+  document.getElementById("button_keyboard").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_voice").style.display = "none";
+  document.getElementById("button_voice").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_content").style.display = "none";
+  document.getElementById("button_content").classList.remove("seleccionado");
+}
+
+function ensenar_audio() {
+  document.getElementById("opcion_contenido_multiplayer").style.display = "none";
+  document.getElementById("button_multiplayer").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_audio").style.display = "block";
+  document.getElementById("button_audio").classList.add("seleccionado");
+
+  document.getElementById("opcion_contenido_video").style.display = "none";
+  document.getElementById("button_video").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_aim").style.display = "none";
+  document.getElementById("button_aim").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_keyboard").style.display = "none";
+  document.getElementById("button_keyboard").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_voice").style.display = "none";
+  document.getElementById("button_voice").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_content").style.display = "none";
+  document.getElementById("button_content").classList.remove("seleccionado");
+}
+
+function ensenar_video() {
+  document.getElementById("opcion_contenido_multiplayer").style.display = "none";
+  document.getElementById("button_multiplayer").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_audio").style.display = "none";
+  document.getElementById("button_audio").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_video").style.display = "block";
+  document.getElementById("button_video").classList.add("seleccionado");
+
+  document.getElementById("opcion_contenido_aim").style.display = "none";
+  document.getElementById("button_aim").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_keyboard").style.display = "none";
+  document.getElementById("button_keyboard").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_voice").style.display = "none";
+  document.getElementById("button_voice").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_content").style.display = "none";
+  document.getElementById("button_content").classList.remove("seleccionado");
+}
+
+function ensenar_aim() {
+  document.getElementById("opcion_contenido_multiplayer").style.display = "none";
+  document.getElementById("button_multiplayer").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_audio").style.display = "none";
+  document.getElementById("button_audio").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_video").style.display = "none";
+  document.getElementById("button_video").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_aim").style.display = "block";
+  document.getElementById("button_aim").classList.add("seleccionado");
+
+  document.getElementById("opcion_contenido_keyboard").style.display = "none";
+  document.getElementById("button_keyboard").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_voice").style.display = "none";
+  document.getElementById("button_voice").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_content").style.display = "none";
+  document.getElementById("button_content").classList.remove("seleccionado");
+}
+
+function ensenar_keyboard() {
+  document.getElementById("opcion_contenido_multiplayer").style.display = "none";
+  document.getElementById("button_multiplayer").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_audio").style.display = "none";
+  document.getElementById("button_audio").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_video").style.display = "none";
+  document.getElementById("button_video").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_aim").style.display = "none";
+  document.getElementById("button_aim").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_keyboard").style.display = "block";
+  document.getElementById("button_keyboard").classList.add("seleccionado");
+
+  document.getElementById("opcion_contenido_voice").style.display = "none";
+  document.getElementById("button_voice").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_content").style.display = "none";
+  document.getElementById("button_content").classList.remove("seleccionado");
+}
+
+function ensenar_voice() {
+  document.getElementById("opcion_contenido_multiplayer").style.display = "none";
+  document.getElementById("button_multiplayer").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_audio").style.display = "none";
+  document.getElementById("button_audio").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_video").style.display = "none";
+  document.getElementById("button_video").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_aim").style.display = "none";
+  document.getElementById("button_aim").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_keyboard").style.display = "none";
+  document.getElementById("button_keyboard").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_voice").style.display = "block";
+  document.getElementById("button_voice").classList.add("seleccionado");
+
+  document.getElementById("opcion_contenido_content").style.display = "none";
+  document.getElementById("button_content").classList.remove("seleccionado");
+}
+
+function ensenar_content() {
+  document.getElementById("opcion_contenido_multiplayer").style.display = "none";
+  document.getElementById("button_multiplayer").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_audio").style.display = "none";
+  document.getElementById("button_audio").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_video").style.display = "none";
+  document.getElementById("button_video").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_aim").style.display = "none";
+  document.getElementById("button_aim").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_keyboard").style.display = "none";
+  document.getElementById("button_keyboard").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_voice").style.display = "none";
+  document.getElementById("button_voice").classList.remove("seleccionado");
+
+  document.getElementById("opcion_contenido_content").style.display = "block";
+  document.getElementById("button_content").classList.add("seleccionado");
+}
